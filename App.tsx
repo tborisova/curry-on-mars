401a1627943990192cd4c71d7852d90fd4656891
Tsvetelina Borisova <ts.borisova3@gmail.com>
2024-01-28 16:25:28 +0200

    Add second screen for showing list of recipes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import {
+  FlatList,
   Pressable,
   SafeAreaView,
   StyleSheet,
@@ -9,9 +10,75 @@ import {
   View,
 } from "react-native";
 
-export default function App() {
-  const [text, onChangeText] = React.useState<string>("");
+const RECIPES = [
+  {
+    title: "Eggs with olives",
+    description: "Mix 1 egg with olives",
+    id: "1",
+  },
+  {
+    title: "Eggs with tomatoes",
+    description: "Mix tomatoes with eggs",
+    id: "2",
+  },
+  {
+    title: "Eggs with parsley",
+    description: "Mix eggs with parsley",
+    id: "3",
+  },
+];
 
+type Recipe = { title: string; description: string };
+
+function Item({ item }: { item: Recipe }) {
+  return (
+    <View>
+      <Text>{item.title}</Text>
+      <Text>{item.description}</Text>
+    </View>
+  );
+}
+export default function App() {
+  const [text, onChangeText] = useState<string>("");
+  const [showList, setShowLit] = useState<boolean>(false);
+
+  if (showList) {
+    return (
+      <>
+        <SafeAreaView style={styles.container}>
+          <FlatList
+            data={RECIPES}
+            renderItem={({ item }) => <Item item={item} />}
+            keyExtractor={(item) => item.id}
+            style={styles.list}
+          />
+
+          <Pressable onPress={() => setShowLit(false)}>
+            <Text>Back</Text>
+          </Pressable>
+        </SafeAreaView>
+      </>
+    );
+  } else {
+    return (
+      <InitialScreen
+        text={text}
+        onChangeText={onChangeText}
+        setShowList={setShowLit}
+      />
+    );
+  }
+}
+
+function InitialScreen({
+  text,
+  onChangeText,
+  setShowList,
+}: {
+  text: string;
+  onChangeText: Dispatch<SetStateAction<string>>;
+  setShowList: Dispatch<SetStateAction<boolean>>;
+}) {
   return (
     <View style={styles.container}>
       <Text>Hello, what do you have in your fridge?</Text>
@@ -25,7 +92,7 @@ export default function App() {
 
       <SafeAreaView>
         <Pressable
-          onPress={() => alert(`searching for ${text}`)}
+          onPress={() => setShowList(true)}
           style={[styles.buttonLabel]}
         >
           <Text style={[styles.searchText]}>Search for recipes</Text>
@@ -57,5 +124,9 @@ const styles = StyleSheet.create({
   },
   searchText: {
     color: "white",
+  },
+  list: {
+    borderColor: "red",
+    borderWidth: 12,
   },
 });
